c9eecd3afe04d534553e2da6e9c001a0713a7b30
Leo-vF <vonfoerster.leo@gmail.com>
2021-10-01 15:38:57 +0200

    added filtering for calibration frames

--- picture-sort.py
+++ picture-sort.py
@@ -34,5 +34,19 @@ def rem(files):
         os.remove(f"NEF/{filename}.NEF")
 
 
+@cli.command()
+@click.argument("cal-type", type=click.Choice(["dark", "bias", "flat"]))
+@click.argument("files", nargs=-1)
+def cal(cal_type, files):
+    try:
+        os.makedirs("JPG/{}".format(cal_type))
+        os.makedirs("NEF/{}".format(cal_type))
+    except FileExistsError:
+        print("The subfolders already exist. Proceeding to move files into pre-existing folders.")
+    for file in files:
+        filename = f"DSC_{file.zfill(4)}"
+        shutil.move(f"JPG/{filename}.JPG", f"JPG/{cal_type}/")
+        shutil.move(f"NEF/{filename}.NEF", f"NEF/{cal_type}/")
+
 if __name__ == "__main__":
     cli()
